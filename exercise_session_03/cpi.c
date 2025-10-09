880d3be3e8e2829cdad64e7c4fc7d077dd7df3b4
Kasper Shaun Powell <kpowell@eiger-ln002.cscs.ch>
2025-10-09 23:34:08 +0200

    Assignment 3 Part 1

---
+++ exercise_session_03/cpi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "get_time.h"
+
+static long nSteps = 1000000000;
+static double PI25DT = 3.141592653589793238462643;
+
+
+int main(int argc, char** argv){
+    double pi, h, sum, x;
+    double tStart, tElapsed;
+
+	h   = 1.0 / (double) nSteps;
+	sum = 0.0;
+    tStart = getTime();
+
+	for (long i = 0 ; i < nSteps; i ++) {
+	    x = h * ((double)i - 0.5);
+	    sum += 4.0 / (1.0 + x*x);
+	}
+	pi = h * sum;
+    
+    tElapsed = getTime() - tStart;
+	printf("pi is approximately %.16f, Error is %.16f\n",
+		        pi, pi - PI25DT);
+    printf("Computed in %.4g seconds\n", tElapsed);
+    return 0;
+}
---
+++ exercise_session_03/get_time.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+
+double getTime(void) {
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv, &tz);
+    return tv.tv_sec + 1e-6*(double)tv.tv_usec;
+}
+
+
+
+
