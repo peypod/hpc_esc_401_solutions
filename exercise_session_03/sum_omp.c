628208e746d710fcad8964d89562abcdf1a97904
Kasper Shaun Powell <kpowell@eiger-ln004.cscs.ch>
2025-10-10 13:01:05 +0200

    final changes

---
+++ exercise_session_03/sum_omp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "get_time.h"
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float r2;
+} coord;
+
+int main(int argc, char *argv[]) {
+    const int N = 1024*1024*256;
+    coord *data;
+    int i;
+    double sum;
+    double startTime, stopTime, elapsedTime;    
+
+    startTime = getTime();
+
+    data = malloc(N * sizeof(coord));
+    assert(data);
+    
+    #pragma omp parallel for reduction(+ : sum)
+    for(i=0; i<N; ++i) {
+        data[i].x = i & 31;
+        data[i].y = i & 63;
+        data[i].z = i & 15;
+        data[i].r2 = data[i].x*data[i].x + data[i].y*data[i].y + data[i].z*data[i].z;
+        sum += sqrt(data[i].r2);
+    }
+    printf("sum=%f\n", sum);
+
+    
+    stopTime = getTime();
+    elapsedTime = stopTime - startTime;
+    
+    printf("elapsed local Time: %.4g seconds\n", elapsedTime);
+
+    return 0;
+}
